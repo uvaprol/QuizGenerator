22efec280eae7a67c7529351ec49b8c6c0c17a1f
uvaprol <koryakin_ad@mail.ru>
2025-02-03 01:34:06 +0300

    main

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from flask import Flask, render_template, request, jsonify
-from seting import *
+# from seting import *
 import sqlite3
 
 DEV_MODE = False
 app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+@app.route('/quiz')
+def get_quiz():
+    pass
+
+@app.route('/registration')
+def registration():
+    return render_template('registration.html')
+
+@app.route('/setting')
+def quiz_setting():
+    pass
 
 app.run(host='0.0.0.0', port=80, debug=DEV_MODE)
