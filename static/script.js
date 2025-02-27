145d5f07c0008ceeff86be8c850ac0d5c86b1de9
uvaprol <koryakin_ad@mail.ru>
2025-02-27 18:23:17 +0300

    ditails work

--- static/script.js
+++
@@ -1,9 +0,0 @@
-const inputs = document.getElementsByTagName('input')
-for (input of inputs){
-    input.addEventListener('input', function(){
-        document.getElementById(`mirrow_${this.id.slice(6)}`).innerHTML = this.value
-    })
-    input.onselect = function(){
-
-    }
-}
