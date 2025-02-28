61c0cc9620e484f56d33e19ec8f79896c9a22fcd
uvaprol <koryakin_ad@mail.ru>
2025-02-28 18:57:41 +0300

    add buttom creating

--- static/script/script.js
+++ static/script/script.js
@@ -1,20 +1,34 @@
-const inputs = document.getElementsByClassName('input')
 const mods = document.getElementsByClassName('mod')
+const sections = document.getElementsByTagName('main')[0].getElementsByTagName('section')
 
-for (e of inputs){
-    e.addEventListener('focus', function (){
-        if (this.innerText == 'Вопрос...' || this.innerText == 'Ответ...'){
-            this.innerText = ''
-            this.style.borderBottom = '1px #4da33c solid'
-        }
-    })
-    e.addEventListener('blur', function (){
-        if (this.innerText == '' || this.innerHTML == '<br>'){
-            this.getAttribute('name') == 'question' ? this.innerText = 'Вопрос...' : this.innerText = 'Ответ...'
-            this.style.borderBottom = 'none'
-        }
-    })
+function InputsMenager(){
+    const inputs = document.getElementsByClassName('input')
+    for (e of inputs){
+        e.addEventListener('focus', function (){
+            if (this.innerText == 'Вопрос...' || this.innerText == 'Ответ...'){
+                this.innerText = ''
+                this.style.borderBottom = '1px #4da33c solid'
+                
+            }
+        })
+        e.addEventListener('blur', function (){
+            if (this.innerText == '' || this.innerHTML == '<br>'){
+                this.getAttribute('name') == 'question' ? this.innerText = 'Вопрос...' : this.innerText = 'Ответ...'
+                this.style.borderBottom = 'none'
+                alert()
+            }
+        })
+    }
 }
+
+function addAnswer(n){
+                    // InputsMenager()
+                                // sections[this.id].removeChild(sections[this.id].getElementsByClassName('answers')[sections[this.id].getElementsByClassName('answers').length - 1])
+    sections[n].getElementsByClassName('answers')[0].innerHTML += 
+    sections[n].getElementsByClassName('answers')[0].getElementsByTagName('template')[0].innerHTML
+
+}
+
 
 for (e of mods){
     e.addEventListener('mouseover', function (){
@@ -26,3 +40,5 @@ for (e of mods){
         this.getElementsByTagName('select')[0].style.display = 'none'
     })
 }
+
+InputsMenager()
