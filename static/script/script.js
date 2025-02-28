fa3db081d73b30f63992c94c548ab195fb83fed5
uvaprol <koryakin_ad@mail.ru>
2025-02-28 17:39:14 +0300

    input border

--- static/script/script.js
+++ static/script/script.js
@@ -4,13 +4,14 @@ const mods = document.getElementsByClassName('mod')
 for (e of inputs){
     e.addEventListener('focus', function (){
         if (this.innerText == 'Вопрос...' || this.innerText == 'Ответ...'){
-            this.innerText = '\u00A0'
+            this.innerText = ''
+            this.style.borderBottom = '1px #4da33c solid'
         }
     })
     e.addEventListener('blur', function (){
-        if (this.innerText == '\u00A0' || this.innerHTML == '<br>'){
-            console.log(this.id)
+        if (this.innerText == '' || this.innerHTML == '<br>'){
             this.getAttribute('name') == 'question' ? this.innerText = 'Вопрос...' : this.innerText = 'Ответ...'
+            this.style.borderBottom = 'none'
         }
     })
 }
