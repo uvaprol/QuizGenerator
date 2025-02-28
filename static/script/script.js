5e412ca4765be4d8a3597816b039bbb57a643eab
uvaprol <koryakin_ad@mail.ru>
2025-02-28 16:27:40 +0300

    js modify

--- static/script/script.js
+++ static/script/script.js
@@ -1,14 +1,16 @@
 const inputs = document.getElementsByClassName('input')
 
 for (e of inputs){
-    e.addEventListener('click', function (){
-        if (e.innerText == 'Вопрос...'){
-            e.innerText = ''
+    e.addEventListener('focus', function (){
+        if (this.innerText == 'Вопрос...' || this.innerText == 'Ответ...'){
+            this.innerText = '\u00A0'
         }
     })
     e.addEventListener('blur', function (){
-        if (e.innerText == '' | e.innerHTML == '<br>'){
-            e.innerText = 'Вопрос...'
+        if (this.innerText == '\u00A0' || this.innerHTML == '<br>'){
+            console.log(this.id)
+            this.getAttribute('name') == 'question' ? this.innerText = 'Вопрос...' : this.innerText = 'Ответ...'
         }
     })
 }
+
