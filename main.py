bd2a8de7cd6691eae27f20ac372f9f1e74d0f061
uvaprol <koryakin_ad@mail.ru>
2025-03-23 20:18:06 +0300

    set data

--- main.py
+++ main.py
@@ -9,9 +9,17 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
-@app.route('/quiz')
-def get_quiz():
-    pass
+@app.route('/quiz/<str:quiz_id>', methods=['GET'])
+def get_quiz(quiz_id):
+    data: dict
+    return render_template('quiz.html', data)
+
+@app.route('/quiz', methods=['POST'])
+def add_quiz():
+    if request.method == 'POST':
+        data = request.form.get('Quize')
+        print(data)
+        return 'success', 200
 
 @app.route('/registration')
 def registration():
--- static/script/send.js
+++ static/script/send.js
@@ -7,16 +7,35 @@ function send(){
         for (input of inputs){
             if (key){
                 key = false
-                data[i] = {
-                    'question': input.innerHTML.trim(), 
-                    'mod': section.querySelector('input').checked, 
-                    'selfAnswer': section.getElementsByClassName('bottom-side')[0].querySelector('input').checked,
-                    'answers': [],
+                if (input.innerHTML.trim() != 'Вопрос...'){
+                    data[i] = {
+                        'question': input.innerHTML.trim(),
+                        'mod': section.querySelector('input').checked,
+                        'selfAnswer': section.getElementsByClassName('bottom-side')[0].querySelector('input').checked,
+                        'answers': [],
+                    }
                 }
                 }
-            data[i]['answers'].push(input.innerHTML.trim())
+            if (input.innerHTML.trim() != 'Ответ...'){
+                data[i]['answers'].push(input.innerHTML.trim())
+            }
         }
         i++
     }
     console.log(data)
+    if (data != {}){
+        $.ajax({
+            url: '/quiz',
+            type: 'POST',
+            data: {
+                'Quize': JSON.stringify(data),
+            },
+            success: (response) => {
+                console.log(response)
+            },
+            error: (response) => {
+                console.log(response)
+            }
+        });
+    }
 }
